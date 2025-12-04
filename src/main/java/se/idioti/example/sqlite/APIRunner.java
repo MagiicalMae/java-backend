e5ca87c7a59d74f282853446f0dab154f78e04ab
MagiicalMae <ellenmae.lantz@gmail.com>
2025-12-05 00:38:25 +0100

    uh yeah

--- src/main/java/se/idioti/example/sqlite/APIRunner.java
+++ src/main/java/se/idioti/example/sqlite/APIRunner.java
@@ -2,7 +2,13 @@ package se.idioti.example.sqlite;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import io.javalin.Javalin;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.CreatedResponse;
+import io.javalin.http.NotFoundResponse;
+
+import java.util.List;
 
 /**
  * This demonstrates how to expose the storage through a REST API using Spark.
@@ -29,11 +35,42 @@ public class APIRunner {
 
 	public static void main(String[] args) throws Exception {
 		APIRunner runner = new APIRunner();
-		Javalin app = Javalin.create(config -> {})
-				// A demonstration of how to use code within an endpoint
-				.get("/", ctx -> { ctx.html("Hello, World!"); })
-				// Run the server on port 5000
-				.start(5000);
+		Javalin app = Javalin.create(config -> {}).start(5000);
+
+        app.get("/", ctx -> {
+            List<Unicorn> unicornList = runner.storage.fetchUnicorns();
+
+            ctx.contentType("application/json");
+            ctx.result(runner.gson.toJson(unicornList));
+        });
+
+        app.get("/{id}", ctx -> {
+            Unicorn unicorn = runner.storage.fetchUnicorn(Integer.parseInt(ctx.pathParam("id")));
+            if (unicorn.id == 0) {
+                throw new NotFoundResponse();
+            }
+
+            ctx.contentType("application/json");
+            ctx.result(runner.gson.toJson(unicorn));
+        });
+
+        app.post("/", ctx -> {
+            try {
+                Unicorn unicorn = runner.gson.fromJson(ctx.body(), Unicorn.class);
+                runner.storage.addUnicorn(unicorn);
+                throw new CreatedResponse();
+            } catch (JsonSyntaxException e) {
+                throw new BadRequestResponse();
+            }
+        });
+
+        app.put("/{id}", ctx -> {
+            //Update unicorn
+        });
+
+        app.delete("/{id}", ctx -> {
+            //Delete unicorn
+        });
 	}
 
 }
